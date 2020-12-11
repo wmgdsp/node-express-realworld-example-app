f2a1e936d1585e4b2c03f17fa8a00a52e1da3ec2
Steffen Hiller <6301+steffen@users.noreply.github.com>
2020-12-11 10:08:10 +0100

    Add standardjs linter and fix all linting issues

--- app.js
+++ app.js
@@ -1,84 +1,88 @@
-var http = require('http'),
-    path = require('path'),
-    methods = require('methods'),
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    session = require('express-session'),
-    cors = require('cors'),
-    passport = require('passport'),
-    errorhandler = require('errorhandler'),
-    mongoose = require('mongoose');
+// const http = require('http')
+const path = require('path')
+// const methods = require('methods')
+const express = require('express')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const cors = require('cors')
+// const passport = require('passport')
+const errorhandler = require('errorhandler')
+const mongoose = require('mongoose')
 
-var isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production'
 
 // Create global app object
-var app = express();
+const app = express()
 
-app.use(cors());
+app.use(cors())
 
 // Normal express config defaults
-app.use(require('morgan')('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(require('morgan')('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.use(require('method-override')());
-app.use(express.static(__dirname + '/public'));
+app.use(require('method-override')())
+app.use(express.static(path.join(__dirname, '/public')))
 
-app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
+app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }))
 
 if (!isProduction) {
-  app.use(errorhandler());
+  app.use(errorhandler())
 }
 
-if(isProduction){
-  mongoose.connect(process.env.MONGODB_URI);
+if (isProduction) {
+  mongoose.connect(process.env.MONGODB_URI)
 } else {
-  mongoose.connect('mongodb://localhost/conduit');
-  mongoose.set('debug', true);
+  mongoose.connect('mongodb://localhost/conduit')
+  mongoose.set('debug', true)
 }
 
-require('./models/User');
-require('./models/Article');
-require('./models/Comment');
-require('./config/passport');
+require('./models/User')
+require('./models/Article')
+require('./models/Comment')
+require('./config/passport')
 
-app.use(require('./routes'));
+app.use(require('./routes'))
 
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.use(function (req, res, next) {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 /// error handlers
 
 // development error handler
 // will print stacktrace
 if (!isProduction) {
-  app.use(function(err, req, res, next) {
-    console.log(err.stack);
+  app.use(function (err, req, res, next) {
+    console.log(err.stack)
 
-    res.status(err.status || 500);
+    res.status(err.status || 500)
 
-    res.json({'errors': {
-      message: err.message,
-      error: err
-    }});
-  });
+    res.json({
+      errors: {
+        message: err.message,
+        error: err
+      }
+    })
+  })
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({'errors': {
-    message: err.message,
-    error: {}
-  }});
-});
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500)
+  res.json({
+    errors: {
+      message: err.message,
+      error: {}
+    }
+  })
+})
 
 // finally, let's start our server...
-var server = app.listen( process.env.PORT || 3000, function(){
-  console.log('Listening on port ' + server.address().port);
-});
+const server = app.listen(process.env.PORT || 3000, function () {
+  console.log('Listening on port ' + server.address().port)
+})
